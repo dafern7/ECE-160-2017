3f21c785d1d243330e097f1b6897454a163ef45f
Richard Fu <richard.fu123@gmail.com>
2017-09-14 13:23:43 -0400

    included number limit warning

--- calc.c
+++ calc.c
@@ -1,7 +1,8 @@
+#include<stdio.h>
+
  /*will attempt to include a repeated operation loop to allow users to continue operating.
 	as of now, user can only perform one operation before program closes*/
-
-#include<stdio.h>
+/* note that users can only operate up to a value of 4294967296, as the operating system is 32 bit.*/
 
 int main()
 {
@@ -9,32 +10,32 @@ int main()
 	int mult1, mult2, add1, add2, sub1, sub2, m, a, s;
 	float div1, div2, d;
 
-	printf("Enter an operation with or without the numbers you wish to calculate.\nBe sure to include spaces between each number!: ");
+	printf("Enter an operation followed by the numbers you wish to use in order.\nBe sure to include spaces between each number!: ");
 	scanf_s("%c", &op, 1); //get input operation
 		switch (op) { 
 			case'x':
-				printf("You have chosen multiplication\n What two numbers would you like to multiply? ");
+				printf("You have chosen multiplication\nHere is the result: ");
 				scanf_s("%d", &mult1); //get input numbers
 				scanf_s("%d", &mult2);
 				m = mult1 * mult2;
 				printf("%d*%d=%d\n", mult1, mult2, m);
 				break;
 			case '+':
-				printf("You have chosen addition\nWhat two numbers would you like to add? ");
+				printf("You have chosen addition\nHere is the result: ");
 				scanf_s("%d", &add1);
 				scanf_s("%d", &add2);
 				a = add1 + add2;
 				printf("%d+%d=%d\n", add1, add2, a);
 				break;
 			case '-':
-				printf("You have chosen subtraction\nWhat two numbers would you like to subtract? ");
+				printf("You have chosen subtraction\nHere is the result: ");
 				scanf_s("%d", &sub1);
 				scanf_s("%d", &sub2);
 				s = sub1 - sub2;
 				printf("%d-%d=%d\n", sub1, sub2, s);
 				break;
 			case '/':
-				printf("You have chosen division\nWhat two numbers would you like to divide? ");
+				printf("You have chosen division\nHere is the result: ");
 				scanf_s("%f", &div1);
 				scanf_s("%f", &div2);
 				d = div1 / div2;
